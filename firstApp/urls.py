9bb5f03022c891a9945d6c51a8e19cecfb838311
Ashish Yadav <savitayadavmansarover@gmail.com>
2023-11-29 22:52:14 +0530

    done with basic html response views.py templates use url and all

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+#here all the urls are linked
 from django.contrib import admin
 from django.urls import path
 from home.views import home,success_page
--- home/views.py
+++ home/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello, I am the home page</h1>")
+    # return HttpResponse("<h1>Hello, I am the home page</h1>")
+    return render(request,"index.html")
 
 
 def success_page(request):
-    return HttpResponse("<h1>Hey this is a success page</h1>")
+    # return HttpResponse("<h1>Hey this is a success page</h1>")
+    return render(request,"success/index.html")
