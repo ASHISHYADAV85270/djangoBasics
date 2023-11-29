23a8a94854014fd91978c0dee42ef08f6d3b1cdc
Ashish Yadav <savitayadavmansarover@gmail.com>
2023-11-29 23:22:16 +0530

    done with basic of sending data from backend to front end using context

--- home/views.py
+++ home/views.py
@@ -3,9 +3,13 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 # Create your views here.
+# The home view renders the index.html template and passes a list of people as the 'peoples' context variable.
 def home(request):
     # return HttpResponse("<h1>Hello, I am the home page</h1>")
-    return render(request,"index.html")
+    peoples=[{'name':"ashish","age":10,},{'name':"Harsh","age":22,},{'name':"palak","age":25,}]
+    
+    #context is used to send data from this views to html
+    return render(request,"index.html",context={'peoples':peoples})
 
 
 def success_page(request):
